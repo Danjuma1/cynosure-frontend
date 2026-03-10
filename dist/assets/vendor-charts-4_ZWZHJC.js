f6f0bd6b91b2cfb98e1b1999a0462af8c04e92f8
Danjuma1 <hda0202@gmail.com>
2026-03-11 00:17:53 +0100

    new build

---
+++ dist/assets/vendor-charts-4_ZWZHJC.js
@@ -0,0 +1,1 @@
+import"./vendor-query-Dij8RlNo.js";import"./vendor-react-DFC1aH6G.js";
--- vite.config.js
+++ vite.config.js
@@ -16,15 +16,28 @@ export default defineConfig({
       '@assets': path.resolve(__dirname, './src/assets'),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-react': ['react', 'react-dom', 'react-router-dom'],
+          'vendor-query': ['@tanstack/react-query'],
+          'vendor-ui': ['framer-motion', '@headlessui/react', '@heroicons/react'],
+          'vendor-charts': ['chart.js', 'react-chartjs-2'],
+          'vendor-utils': ['axios', 'zustand', 'react-hook-form', 'react-hot-toast', 'clsx', 'date-fns'],
+        },
+      },
+    },
+  },
   server: {
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://cynosure-api.gravitylab.com.ng',
         changeOrigin: true,
       },
       '/ws': {
-        target: 'ws://localhost:8001',
+        target: 'ws://cynosure-api.gravitylab.com.ng/ws',
         ws: true,
       },
     },
